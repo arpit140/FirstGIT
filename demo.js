c193e1b145874ce55219642cdfa732b7f71426b6
arpit140 <arpitsunny6@gmail.com>
2024-01-05 10:14:22 +0530

    Rename file: index.js -> demo.js

---
+++ demo.js
@@ -0,0 +1,14 @@
+
+const MainHeading=document.getElementById('main-heading')
+MainHeading.textContent='Fruit World'
+
+const header=document.getElementById('header')
+header.style.backgroundColor='green'
+header.style.color='orange'
+header.style.borderBottom='3px solid orange'
+
+const basketHeading=document.getElementById('basket-heading')
+basketHeading.style.color='green'
+
+const thanks=document.getElementById('thanks')
+thanks.innerHTML='<p>Please visit us again</p>'
