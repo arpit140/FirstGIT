c6d918fc8934e8aa64d2c9b305e43dae9c68f57b
arpit140 <arpitsunny6@gmail.com>
2024-01-05 10:33:57 +0530

    update file with new codes

--- demo.js
+++ demo.js
@@ -12,3 +12,9 @@ basketHeading.style.color='green'
 
 const thanks=document.getElementById('thanks')
 thanks.innerHTML='<p>Please visit us again</p>'
+
+const fruit=document.getElementsByClassName('fruit')
+fruit[2].style.backgroundColor='yellow'
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.fontWeight='bold'
+}
